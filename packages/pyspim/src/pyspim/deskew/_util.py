e073cd097620c17fb1f758a382d7fb016b89b018
wolfffff <22243650+Wolfffff@users.noreply.github.com>
2025-07-19 15:35:57 -0400

    Doccccs

--- packages/pyspim/src/pyspim/deskew/_util.py
+++ packages/pyspim/src/pyspim/deskew/_util.py
@@ -3,6 +3,19 @@ import numpy
 
 # yields the coefficients from highest to lowest degree
 def extract_coefficients(p, degree):
+    """Extract polynomial coefficients from a polynomial function.
+    
+    Given a polynomial function p(x) of specified degree, this function
+    extracts the coefficients by evaluating the polynomial at n+1 points
+    and solving a linear system to find the coefficients.
+    
+    Args:
+        p (callable): A polynomial function that takes a single argument.
+        degree (int): The degree of the polynomial.
+        
+    Returns:
+        numpy.ndarray: Array of coefficients from highest to lowest degree.
+    """
     n = degree + 1
     sample_x = [x for x in range(n)]
     sample_y = [p(x) for x in sample_x]
@@ -15,4 +28,14 @@ def extract_coefficients(p, degree):
 
 
 def characteristic_polynomial(A):
+    """Compute the characteristic polynomial of a matrix.
+    
+    This function is currently a placeholder and not implemented.
+    
+    Args:
+        A (numpy.ndarray): Input matrix.
+        
+    Returns:
+        NotImplemented: This function is not yet implemented.
+    """
     pass
